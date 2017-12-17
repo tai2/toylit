1b355c8af8cc3fc7c7a3f47ffc488eac64fceb20
tai2 <hello@tai2.net>
2017-12-18 01:25:14 +0900

    Add a test case.

---
+++ test/simple.md
@@ -0,0 +1,21 @@
+# サンプル文書
+
+## <<構成>>
+
+```javascript
+<要素1>
+<要素2>
+```
+
+## <要素1>
+
+```javascript
+console.log('elem 1')
+```
+
+## <要素2>
+
+```javascript
+console.log('elem 2')
+```
+
--- test/test.js
+++ test/test.js
@@ -2,7 +2,17 @@ import test from 'ava';
 import fs from 'fs';
 import compile from '../compile'
 
-test('comile function results expected code', t => {
+test('comile function results expected code(simple.md)', t => {
+  const text = fs.readFileSync('./test/simple.md', 'utf8')
+  const code = compile(text)
+  const expected = `console.log('elem 1')
+console.log('elem 2')
+`
+  t.is(code, expected);
+});
+
+
+test('comile function results expected code(complex.md)', t => {
   const text = fs.readFileSync('./test/complex.md', 'utf8')
   const code = compile(text)
   const expected = `console.log('elem 1')
